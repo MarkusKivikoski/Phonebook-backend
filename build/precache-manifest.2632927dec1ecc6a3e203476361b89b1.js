1898690ccc1d7e2089810cc4ec6e4d123fbf9eba
MarkusKivikoski <markus.kivikoski@gmail.com>
2020-08-14 15:39:31 +0300

    another ting

---
+++ build/precache-manifest.2632927dec1ecc6a3e203476361b89b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58e8c146880eb487ed536bfed548dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55cf729ead7976ca28b",
+    "url": "/static/css/main.92709fd9.chunk.css"
+  },
+  {
+    "revision": "4716da87646028f936c7",
+    "url": "/static/js/2.39147d51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39147d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55cf729ead7976ca28b",
+    "url": "/static/js/main.123b3ec8.chunk.js"
+  },
+  {
+    "revision": "8bf532e24808fa2d295c",
+    "url": "/static/js/runtime-main.8208f4ca.js"
+  }
+]);
